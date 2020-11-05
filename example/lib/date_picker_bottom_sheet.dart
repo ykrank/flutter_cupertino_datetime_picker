e101c8fe1164bcd69b24c64d1416ed2ccecf90cb
ykrank <ykrank@gmail.com>
2020-11-05 09:49:54 +0800

    fix example import

--- example/lib/date_picker_bottom_sheet.dart
+++ example/lib/date_picker_bottom_sheet.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_cupertino_date_picker/flutter_cupertino_date_picker.dart';
+import 'package:flutter_cupertino_datetime_picker/flutter_cupertino_date_picker.dart';
 
 class DatePickerBottomSheet extends StatefulWidget {
   DatePickerBottomSheet({Key key}) : super(key: key);
